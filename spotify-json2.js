99006639d031308d7c3f5d4fdf28b1b232afd099
warlock4030 <59950367+warlock4030@users.noreply.github.com>
2026-08-26 10:36:18 +0800

    Create spotify-json2.js

---
+++ spotify-json2.js
@@ -0,0 +1,25 @@
+let url = $request.url;
+const method = $request.method;
+console.log(`spotifyjson2-2026.03.04`);
+if (!$response.body) {
+    console.log(`$response.body为undefined:${url}`);
+    $done({});
+}
+let body = JSON.parse($response.body);
+
+if (url.includes("/device-capabilities/v1/capabilities")) {
+    console.log('capabilities');
+    body.effective_license = 'premium';
+    //body.audio_quality = 'HIFI_24';
+    if (!body.supports_hifi?.fully_supported) {
+        body.supports_hifi.fully_supported = true;
+    }
+    if (!body.supports_hifi?.user_eligible) {
+        body.supports_hifi.user_eligible = true;
+    }
+}
+
+body = JSON.stringify(body);
+$done({
+    body
+});
